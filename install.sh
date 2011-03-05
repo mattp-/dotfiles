f67e0c838eb19a78f835b757e933d04448568fa6
Matt Phillips <mattp@cpan.org>
2011-03-05 12:26:08 -0500

    Initial drop

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+echo $SELF_PATH
+
+ruby vim-update-bundles
+
+ln -sf $SELF_PATH/bashrc ~/.bashrc
+ln -sf $SELF_PATH/screenrc ~/.screenrc
+
+rm -rf ~/.vim
+ln -sf $SELF_PATH/vim/vimrc ~/.vimrc
+ln -sf $SELF_PATH/vim ~/.vim
+
+ln -sf $SELF_PATH/gitconfig ~/.gitconfig
+
+ln -sf $SELF_PATH/xinitrc ~/.xinitrc
+ln -sf $SELF_PATH/Xdefaults ~/.Xdefaults
+ln -sf $SELF_PATH/gtkrc-2.0 ~/.gtkrc-2.0
+
+# fix perl-support path hardcode bug with pathogen
+# Local template file '/home/mattp/.vim/perl-support/templates/Templates' not readable.
+ln -sf $SELF_PATH/vim/bundle/perl-support.vim/perl-support ~/.vim/
+rm $SELF_PATH/vim/bundle/perl-support.vim/doc/tags-te
---
+++ laptop.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+OUTPUT=""
+WLANDEV='wlan0'
+ESSID=`/sbin/iwconfig $WLANDEV | grep ESSID | sed -e 's/.*ESSID:\"//; s/\".*$//'`
+SIGQUAL=`cat /sys/class/net/$WLANDEV/wireless/link`
+OUTPUT="[$OUTPUT $ESSID:$SIGQUAL% ]  ["
+PWRDIR=/sys/class/power_supply
+for i in `ls --indicator-style=none -1 $PWRDIR \
+          | grep BAT \
+          | sed -e 's/BAT//'`;
+do
+  THISBATT="$PWRDIR/BAT$i"
+  CAP=`cat $THISBATT/energy_full`
+  CUR=`cat $THISBATT/energy_now`
+  DRN=`cat $THISBATT/current_now`
+  STS=`cat $THISBATT/status`
+  PRC=`echo $CUR 100 \* $CAP / p | dc`
+  if [ $STS == "Discharging" ]; then
+    HRS=`echo $CUR $DRN / p | dc`
+    MIN=`echo 2 k $CUR $DRN / p | dc`
+      MIN=`echo 2 k $MIN $HRS - 60 \* p | dc`
+      MIN=`echo $MIN 1 / p | dc`
+      MIN=`printf %02d $MIN`
+    TIME=$HRS:$MIN
+    OUTPUT="$OUTPUT -$TIME"
+  elif [ $STS == "Charging" ]; then
+    OUTPUT="$OUTPUT +$PRC%"
+  elif [ $STS == "Full" ]; then
+    OUTPUT="$OUTPUT [+]"
+  elif [ $STS == "Unknown" ]; then
+    OUTPUT="$OUTPUT $PRC%"
+  fi
+  OUTPUT="$OUTPUT ]"
+done
+CLOCK=`date +"%a %d %b %l:%M:%S %P"`
+echo $OUTPUT $CLOCK
